582bbc29ce957ca1d5b1d8e1fc07c1f381218f3c
Leonardo Melo Santos <leonardomelosantos@gmail.com>
2025-05-21 19:59:37 -0300

    Original repository

---
+++ JOIEnergy/Services/AccountService.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using JOIEnergy.Enums;
+
+namespace JOIEnergy.Services
+{
+    public class AccountService : IAccountService
+    { 
+        private Dictionary<string, string> _smartMeterToPricePlanAccounts;
+
+        public AccountService(Dictionary<string, string> smartMeterToPricePlanAccounts) {
+            _smartMeterToPricePlanAccounts = smartMeterToPricePlanAccounts;
+        }
+
+        public string GetPricePlanIdForSmartMeterId(string smartMeterId) {
+            if (!_smartMeterToPricePlanAccounts.ContainsKey(smartMeterId))
+            {
+                return null;
+            }
+            return _smartMeterToPricePlanAccounts[smartMeterId];
+        }
+    }
+}
---
+++ JOIEnergy/Services/IAccountService.cs
@@ -0,0 +1,9 @@
+﻿using JOIEnergy.Enums;
+
+namespace JOIEnergy.Services
+{
+    public interface IAccountService
+    {
+        string GetPricePlanIdForSmartMeterId(string smartMeterId);
+    }
+}
