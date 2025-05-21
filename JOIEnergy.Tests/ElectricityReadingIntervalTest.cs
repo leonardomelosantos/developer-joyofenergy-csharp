7812d10fb03c32fe4b8f144108f4b9d6b53f2e0f
Leonardo Melo Santos <leonardomelosantos@gmail.com>
2025-05-21 20:07:23 -0300

    Added two new helper files

---
+++ JOIEnergy.Tests/ElectricityReadingIntervalTest.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using JOIEnergy.Domain;
+using Xunit;
+
+namespace JOIEnergy.Tests
+{
+    public class ElectricityReadingIntervalTest
+    {
+        public ElectricityReadingIntervalTest()
+        {
+        }
+
+        [Fact]
+        public void GetLastWeekFromToday()
+        {
+            Assert.Equal(new DateTime(2021, 9, 26), ElectricityReadingInterval.getPreviousWeekInterval(new DateTime(2021, 10, 5, 12, 00, 00)).Start);
+            Assert.Equal(new DateTime(2021, 10, 3), ElectricityReadingInterval.getPreviousWeekInterval(new DateTime(2021, 10, 5, 12, 00, 00)).End);
+        }
+    }
+}
